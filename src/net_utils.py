71a5c04d2d347306eab7669eb7ad7d14dd913d8b
wang.weiran <wang.wei.ran@outlook.com>
2022-12-31 21:09:43 +1100

    add blog_client conf

--- src/net_utils.py
+++ src/net_utils.py
@@ -5,7 +5,6 @@ def connect_socket(host, port) -> socket:
     # 解析DNS获取对应协议簇、socket类型、目标地址
     # getaddrinfo -> [(family, socket_type, proto, canonname, target_addr),]
     (family, socket_type, _, _, target_addr) = socket.getaddrinfo(host, port)[0]
-    print(target_addr)
     tmp_socket = socket.socket(family, socket_type)
     tmp_socket.setblocking(False)
     tmp_socket.settimeout(5)
