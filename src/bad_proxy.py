3b25760b6c3f027eb0170f8274c54c135e454af9
wang.weiran <wang.wei.ran@outlook.com>
2023-01-02 04:01:31 +1100

    optimize code structure

--- src/bad_proxy.py
+++ src/bad_proxy.py
@@ -7,20 +7,13 @@ from application_layer import BTPException
 
 
 class BadProxy(object):
-    """
-    简单的HTTP代理
-    客户端(client) <=> 代理端(proxy) <=> 服务端(server)
-    """
     def __init__(self, config: Config):
         self.inbound = Inbound(config.inbound_config)
         self.outbound = Outbound(config.outbound_config)
 
     def proxy(self):
-        """
-        代理核心程序
-        参数：socket_client 代理端与客户端之间建立的套接字
-        """
         try:
+            # Payload is the data from first package
             target_host, target_port, payload = self.inbound.connect()
             if target_host is None or target_port is None:
                 return
@@ -30,44 +23,31 @@ class BadProxy(object):
         except BTPException as e:
             print('invalid btp in connection', e)
             self.inbound.create_fake_connection()
+        except Exception as e:
+            print(e)
         finally:
             self.inbound.close()
             self.outbound.close()
 
-    # 使用select异步处理，不阻塞
+    # async listen with select
     def async_listen(self):
-        """
-        使用select实现异步处理数据
-        参数：socket_client 代理端与客户端之间建立的套接字
-        参数：socket_server 代理端与服务端之间建立的套接字
-        """
-        _rlist = [self.inbound.socket, self.outbound.socket]
         is_recv = True
         while is_recv:
-            try:
-                rlist, _, elist = select.select(_rlist, [], [], 2)
-                if elist:
-                    break
-                for tmp_socket in rlist:
-                    is_recv = True
-                    # 接收数据, 按协议解析，inbound解析request，outbound解析response
-                    # data = tmp_socket.recv(self.inbound.socket_recv_buf_size)
-                    # socket_client状态为readable, 当前接收的数据来自客户端
-                    data = tmp_socket.recv(8192)
-                    if tmp_socket is self.inbound.socket:
-                        if data == b'':
-                            is_recv = False
-                            continue
-                        self.outbound.socket.send(data)  # 将客户端请求数据发往服务端
+            rlist, _, elist = select.select([self.inbound.socket,
+                                             self.outbound.socket],
+                                            [], [], 2)
+            assert len(elist) == 0
+            for sock in rlist:
+                is_recv = True
+                data = sock.recv(8192)
+                # no data, try other socket
+                if data == b'':
+                    is_recv = False
+                    continue
 
-                    elif tmp_socket is self.outbound.socket:
-                        if data == b'':
-                            is_recv = False
-                            continue
-                        self.inbound.socket.send(data)
-                        # print('proxy', 'client <- server')
-
-                # time.sleep(self.delay)  # 适当延迟以降低CPU占用
-            except Exception as e:
-                print(e)
-                break
+                # inbound received, outbound send data
+                if sock is self.inbound.socket:
+                    self.outbound.socket.send(data)
+                # outbound received, inbound send data
+                elif sock is self.outbound.socket:
+                    self.inbound.socket.send(data)
