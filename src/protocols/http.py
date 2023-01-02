3176c00409b1861c0f6115805d1b6b3024ff1f51
wang.weiran <wang.wei.ran@outlook.com>
2023-01-03 07:53:10 +1100

    add assert in http

--- src/protocols/http.py
+++ src/protocols/http.py
@@ -9,6 +9,11 @@ class HttpRequest(object):
     host: bytes
     payload: bytes
 
+    __valid_methods: list[bytes] = [
+        b'GET', b'HEAD', b'POST', b'PUT', b'DELETE',
+        b'CONNECT', b'OPTIONS', b'TRACE', b'PATCH'
+    ]
+
     def __init__(self, data):
         self.__parse(data)
 
@@ -16,9 +21,13 @@ class HttpRequest(object):
         req_line_end = data.find(b'\r\n')
         headers_end = data.find(b'\r\n\r\n')
 
+        assert req_line_end > 0 and headers_end > 0
+
         # request line
         req_line = data[:req_line_end]
         self.method, self.req_uri, self.version = req_line.split()
+
+        assert self.method in self.__valid_methods
 
         # request header
         headers = data[req_line_end + 2: headers_end]
@@ -38,24 +47,24 @@ class HTTP:
             print('inbound received none data')
             return None, None, None
 
-        http_packet = HttpRequest(raw_data)
+        http_request = HttpRequest(raw_data)
 
         # remove proxy hostname
-        tmp = b'%s//%s' % (http_packet.req_uri.split(b'//')[0], http_packet.host)
+        tmp = b'%s//%s' % (http_request.req_uri.split(b'//')[0], http_request.host)
         raw_data = raw_data.replace(tmp, b'')
 
         # HTTP
-        if http_packet.method in [b'GET', b'POST', b'PUT', b'DELETE', b'HEAD']:
+        if http_request.method in [b'GET', b'POST', b'PUT', b'DELETE', b'HEAD']:
             pass
         # HTTPS
-        if http_packet.method == b'CONNECT':
+        if http_request.method == b'CONNECT':
             success_msg = b'%s %d Connection Established\r\nConnection: close\r\n\r\n' \
-                          % (http_packet.version, 200)
+                          % (http_request.version, 200)
             inbound_socket.send(success_msg)
             raw_data = inbound_socket.recv(buff_size)
 
-        server_host, server_port = http_packet.host.split(b':') \
-            if b':' in http_packet.host else (http_packet.host, 80)
+        server_host, server_port = http_request.host.split(b':') \
+            if b':' in http_request.host else (http_request.host, 80)
 
         if isinstance(server_port, bytes):
             server_port = int(server_port.decode())
